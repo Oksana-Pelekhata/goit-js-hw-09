8ee2dc0b8f0cb31216471ec32f57144243a6cad8
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-03-17 01:09:03 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+
+const TIME_INTERVAL = 1000
+
+let intervalId = null;
+
+startBtn.addEventListener('click', handleStartBtnClick)
+stopBtn.addEventListener('click',handleStoptBtnClick )
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function handleStartBtnClick() {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, TIME_INTERVAL);
+    startBtn.disabled = true;
+}
+
+function handleStoptBtnClick() {
+    clearInterval(intervalId)
+    startBtn.disabled = false;
+}
 
